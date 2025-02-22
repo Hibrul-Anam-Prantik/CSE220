597ece0e39a1e6aeb25b6a33689b4a9f1ac71a13
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2025-02-22 23:46:37 +0600

    Setting the Designer Class fot the Lab Tasks

---
+++ src/Lab01/Node.java
@@ -0,0 +1,18 @@
+package Lab01;
+
+class Node{
+
+    Object elem;
+    Node next;
+
+    public Node(Object elem, Node next){
+        this.elem = elem;
+        this.next = next;
+    }
+    
+    //Overloading
+    public Node(Object elem){
+        this.elem = elem;
+        this.next = null;
+    }
+}
