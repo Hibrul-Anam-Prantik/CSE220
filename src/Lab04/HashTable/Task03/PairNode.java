d1d9ace0e0e35ce0af71ae91b1038aebd0b2e5df
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2025-03-15 07:04:58 +0600

    Lab04 > HashTable_Task03 -> Deletion from hashtable

---
+++ src/Lab04/HashTable/Task03/PairNode.java
@@ -0,0 +1,16 @@
+package Lab04.HashTable.Task03;
+
+public class PairNode {
+    
+    Integer key;
+    String value;
+    PairNode next;
+
+    public PairNode( Integer k, String v ){
+        // TO DO
+        key = k;
+        value = v;
+        next = null;
+    }
+    
+}
