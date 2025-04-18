380b8ec3d3eef8dcc64832283c682615157237ba
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2025-04-18 16:12:07 +0600

    Lab06 -> BinaryTree -> Task04 -> Swap Children Nodes

---
+++ src/Lab06/Task4.java
@@ -0,0 +1,25 @@
+package Lab06;
+
+// Complete the swapChild method
+public class Task4 {
+
+    //===================================TASK#4===================================
+    // This method takes only 3 parameters
+    // 1st one is root of the given tree
+    // 2nd one is there to help you count the levels
+    // 3rd one is the level TILL which you need to swap children
+    // All the changes will happen in-place
+    // This method doesn't return anything
+    // You can use extra helper private static methods as per need
+    public static void swapChild( BTNode root, int lvl, int M ){
+        //TO DO
+        if(root != null && lvl != M) {
+            BTNode left = root.left, right = root.right;
+            root.left = right;
+            root.right = left;
+            swapChild(left, lvl + 1, M);
+            swapChild(right, lvl + 1, M);
+        }
+    }
+    //============================================================================
+}
