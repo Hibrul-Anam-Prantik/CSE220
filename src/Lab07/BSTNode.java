a69edfb7e2cd1f0734cf1cb5e333c3078a977cb0
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2025-04-25 19:42:43 +0600

    Lab07 -> BinarySearchTree -> tasks

---
+++ src/Lab07/BSTNode.java
@@ -0,0 +1,11 @@
+package Lab07;
+
+//DO NOT MAKE ANY CHANGES HERE
+class BSTNode {
+    Integer elem;
+    BSTNode left, right;
+
+    public BSTNode(Integer elem) {
+        this.elem = elem;
+    }
+}
