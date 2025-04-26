44580573336decaf67e3f33236e07870784e7433
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2025-04-26 09:59:34 +0600

    Lab07 -> BinarySearchTree -> Task#04 -> Mirror Sum

--- src/Lab07/Task4.java
+++ src/Lab07/Task4.java
@@ -9,7 +9,16 @@ public class Task4 {
     // You can use extra helper private static methods as per need
     public static Integer mirrorSum( BSTNode root ){
         //TO DO
-        return -1; // remove this line
+        if(root == null) return null;
+        return helperMirrorSum(root.left, root.right, 0);
+    }
+    private static Integer helperMirrorSum(BSTNode left, BSTNode right, Integer sum) {
+        if(left == null || right == null) return sum;
+        sum += left.elem + right.elem;
+        BSTNode ll = left.left, rr = right.right, lr = left.right, rl = right.left;
+        sum = helperMirrorSum(ll, rr, sum) ;
+        sum = helperMirrorSum(lr, rl, sum);
+        return sum;
     }
     //===============================================================
 
